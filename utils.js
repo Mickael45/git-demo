3cb88207cef4bfce0e2c98d1e04e74c4626a6852
Mickael <mickaelgomesconsulting@gmail.com>
2023-06-09 17:06:53 +0200

    Adds utils

---
+++ utils.js
@@ -0,0 +1,15 @@
+export function addTwoNumbers(a, b) {
+  return a + b;
+}
+
+export function multiplyTwoNumbers(a, b) {
+  return a * b;
+}
+
+export function subtractTwoNumbers(a, b) {
+  return a - b;
+}
+
+export function divideTwoNumbers(a, b) {
+    return a / b;
+}
